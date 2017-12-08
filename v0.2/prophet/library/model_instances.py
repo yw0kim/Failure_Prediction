9fc01abf15c451050bb8ae972e17ca0cd48ec5c0
choi <superbag2010@naver.com>
2017-12-08 14:34:36 +0000

    v0.2:trainer can get ML's instance

--- v0.2/prophet/library/model_instances.py
+++ v0.2/prophet/library/model_instances.py
@@ -2,26 +2,33 @@
 
 import sys
 import os
+FAILURE_PREDICTION_PATH = os.environ['FAILURE_PREDICTION']
+sys.path.insert(0, os.path.join(FAILURE_PREDICTION_PATH, "library", "data_prepare"))
+
+import data_transform as dt
 import pandas as pd
 
-sys.path.insert(0, os.path.join("./", "library", "data_transform"))
-import data_transform as dt
+ml_tbl_path = os.path.join(".", "library", "ML_TBL.csv")
+pd_ml_tbl = pd.read_csv(ml_tbl_path)
+module_dirpaths = []   # save each module's import path
 
-model_tbl_path = os.path.join(SOURCE_ROOT, "MODEL_TBL.csv")
-module_dirpaths = []
-
-pd_model_tbl = pd.read_csv(model_tbl_path)
-class_obj_dict = dict()
-for index, row in pd_model_tbl.iterrows():
-    module_path = os.path.join(SOURCE_ROOT, row['MODULE_PATH'])
+module_obj_dict = dict()
+for index, row in pd_ml_tbl.iterrows():
+    module_path = os.path.join(FAILURE_PREDICTION_PATH, row['MODULE_PATH'])
+    # get module name
     module_name = os.path.basename(module_path).split('.')[0]
+
+    # add import path for module
     module_dirpath = os.path.dirname(module_path)
     if module_dirpath not in module_dirpaths:
         module_dirpaths.append(module_dirpath)
         sys.path.insert(0, module_dirpath)
 
-    module = __import__(module_name)
-    class_obj_dict[row['MODEL_NAME']] = getattr(module, row['CLASS_NAME'])
+    # import module
+    module_instance = __import__(module_name)
+    module_obj_dict[row['ML_NAME']] = getattr(module_instance, row['CLASS_NAME'])
 
-print(class_obj_dict)
-dt_func_cls = dt.Data_transform()
+print(module_obj_dict)
+
+
+#dt_func_cls = dt.Data_transform()
